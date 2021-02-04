14ac365d8ef89a1e4ef9bba40f0a5a6304168caf
marynoir <okosunmaryeghonghon@gmail.com>
2021-02-04 22:18:17 +0100

    MARY javascript assessment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  
+  var regex = "/[\W_/g";  //declare a variable of regular expression
+  var lowerCase =str.toLowerCase().replace(regex,""); //convert string to lowercase and replace with string
+  var reverseString = lowerCase.split("").reverse().join(""); //split the lowercase into letters
+  return reverseString === lowerCase; //compares the reverseString and lowerCase and returns true if it is a palindrome and false if not a palindrome
 }
 
 function solution(arg) {
